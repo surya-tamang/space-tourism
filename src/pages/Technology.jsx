469da929351130def1842c5baf0de4d37d21d8b0
evil-89789 <tngsurya055@gmail.com>
2024-06-17 19:09:35 +0545

    technology page

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import Heading from "../components/Heading";
+import dataJson from "../data.json";
 
 const Technology = () => {
-  return <div className="technology_page min-h-screen w-full"></div>;
+  const techData = dataJson.technology;
+  const [currentIndex, setCurrentIndex] = useState(0); // Use array destructuring
+  console.log(currentIndex);
+
+  return (
+    <div className="technology_page min-h-screen w-full flex items-start pt-32 flex-col pb-12">
+      <Heading title={"space launch 101"} index={"03"} />
+      <div className="h-auto ml-28 mt-16 flex">
+        <div className="h-auto w-2/12 flex items-start justify-center flex-col gap-6">
+          {techData.map((item, index) => {
+            return (
+              <button
+                key={index}
+                className={`h-16 w-16 ${index===currentIndex ? 'bg-slate-50': 'bg-slate-400'} rounded-full flex items-center justify-center text-3xl`}
+                onClick={() => setCurrentIndex(index)}
+              >
+                {index + 1}
+              </button>
+            );
+          })}
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="w-6/12 text-slate-50 flex flex-col gap-2 uppercase">
+            <p>the terminology...</p>
+            <h1 className="text-7xl">{techData[currentIndex].techName}</h1>
+            <p>{techData[currentIndex].description}</p>
+          </div>
+          <div>
+            <img
+              src={techData[currentIndex].images.portrait}
+              alt={techData[currentIndex].techName}
+              className="w-96"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Technology;
